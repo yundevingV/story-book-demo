4620243038fae40596c2efc65e43aa6af0fcbd51
yundevingV <91381230+yundevingV@users.noreply.github.com>
2025-08-12 18:10:07 +0900

    refactor: Update ButtonProps interface to extend React button props and make label optional

--- src/stories/button/Button.type.d.ts
+++ src/stories/button/Button.type.d.ts
@@ -10,12 +10,9 @@ type ButtonVariant =
 type ButtonSize = "default" | "sm" | "lg";
 type ButtonState = "default" | "loading" | "success" | "error";
 
-interface ButtonProps {
-  label: string;
+interface ButtonProps extends React.ComponentProps<"button"> {
+  label?: string;
   variant?: ButtonVariant;
   size?: ButtonSize;
   state?: ButtonState;
-  className?: string;
-  children?: React.ReactNode;
-  onClick?: () => void;
 }
