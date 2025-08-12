00a3eb6d19ca5f162adfcdbaed21c6f107b61d58
yundevingV <91381230+yundevingV@users.noreply.github.com>
2025-08-12 18:10:13 +0900

    feat: Add Divider component with customizable variants and Storybook stories for demonstration

---
+++ src/stories/Divider/Divider.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+import { cva } from "class-variance-authority";
+
+interface DividerProps extends React.ComponentProps<"div"> {
+  variant?:
+    | "default"
+    | "primary"
+    | "secondary"
+    | "accent"
+    | "info"
+    | "success"
+    | "warning"
+    | "error";
+}
+
+const dividerStyle = cva("w-full h-0.5", {
+  variants: {
+    variant: {
+      default: "bg-default-200",
+      primary: "bg-primary-500",
+      secondary: "bg-secondary-200",
+      accent: "bg-accent-200",
+      info: "bg-info-200",
+      success: "bg-success-200",
+      warning: "bg-warning-200",
+      error: "bg-error-200",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+  },
+});
+
+export const Divider = ({ variant, className, ...props }: DividerProps) => {
+  return (
+    <div className={cn(dividerStyle({ variant }), className)} {...props} />
+  );
+};
