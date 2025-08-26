46e68deb40d52516c08220124c9dd1a54d3b8945
yundevingV <91381230+yundevingV@users.noreply.github.com>
2025-08-26 16:08:07 +0900

    feat: Enhance BadgePage with comprehensive documentation sections for usage, variants, and sizes

--- storybook-demo-test-app/src/app/badge/page.tsx
+++ storybook-demo-test-app/src/app/badge/page.tsx
@@ -1,3 +1,77 @@
+"use client";
+
+import {
+  DocumentationLayout,
+  DocumentationSection,
+  SectionContent,
+} from "@/components/documentation";
+import { Badge } from "@yundeving/story-book-demo-ui";
+
 export default function BadgePage() {
-  return <div>BadgePage</div>;
+  return (
+    <DocumentationLayout
+      title="Badge"
+      description="Displays a badge or a component that looks like a badge."
+    >
+      <DocumentationSection id="basic-usage">
+        <SectionContent
+          preview={<Badge> Badge </Badge>}
+          code={`import { Badge } from "@yundeving/story-book-demo-ui";
+
+<Badge content="1" />`}
+        />
+      </DocumentationSection>
+
+      <DocumentationSection id="variant">
+        <SectionContent
+          preview={
+            <>
+              <Badge variant="primary"> Badge </Badge>
+              <Badge variant="secondary"> Badge </Badge>
+              <Badge variant="success"> Badge </Badge>
+              <Badge variant="warning"> Badge </Badge>
+              <Badge variant="info"> Badge </Badge>
+            </>
+          }
+          code={`import { Badge } from "@yundeving/story-book-demo-ui";
+
+<Badge variant="primary"> Badge </Badge>
+<Badge variant="secondary"> Badge </Badge>
+<Badge variant="success"> Badge </Badge>
+<Badge variant="warning"> Badge </Badge>
+<Badge variant="info"> Badge </Badge>`}
+        />
+      </DocumentationSection>
+
+      <DocumentationSection id="variant">
+        <SectionContent
+          preview={
+            <>
+              <Badge size="sm"> Badge </Badge>
+              <Badge size="md"> Badge </Badge>
+              <Badge size="lg"> Badge </Badge>
+            </>
+          }
+          code={`import { Badge } from "@yundeving/story-book-demo-ui";
+
+<Badge size="sm"> Badge </Badge>
+<Badge size="md"> Badge </Badge>
+<Badge size="lg"> Badge </Badge>`}
+        />
+      </DocumentationSection>
+      <DocumentationSection id="variant">
+        <SectionContent
+          preview={
+            <>
+              <Badge size="md"> Long Long Text Badge </Badge>
+            </>
+          }
+          code={`import { Badge } from "@yundeving/story-book-demo-ui";
+
+<Badge size="md"> Long Long Text Badge </Badge>
+`}
+        />
+      </DocumentationSection>
+    </DocumentationLayout>
+  );
 }
