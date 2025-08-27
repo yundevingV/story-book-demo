cf9b45e62b57f7b1a2a807f27219ef3d52eb6fe3
yundevingV <91381230+yundevingV@users.noreply.github.com>
2025-08-27 15:08:21 +0900

    feat: Enhance InputPage with structured login form and dynamic error handling for email and password inputs

--- storybook-demo-test-app/src/app/input/page.tsx
+++ storybook-demo-test-app/src/app/input/page.tsx
@@ -56,11 +56,11 @@ export default function InputPage() {
     ) {
       setTimeout(() => {
         setSuccessLoginButtonStatus("success");
-      }, 1000);
+      }, API_DELAY);
     } else {
       setTimeout(() => {
         setSuccessLoginButtonStatus("error");
-      }, 1000);
+      }, API_DELAY);
     }
   };
 
@@ -179,7 +179,72 @@ export default function InputPage() {
           }
           code={`import { Input } from "@yundeving/story-book-demo-ui";
 
-<Input type="email" placeholder="Email" error helperText="invalid Email Format" />`}
+<Card className="w-full max-w-sm">
+  <CardHeader>
+    <CardTitle>Login to your account</CardTitle>
+    <CardDescription>
+      Enter your email below to login to your account
+    </CardDescription>
+  </CardHeader>
+  <CardContent className="flex flex-col gap-2">
+    <form>
+      <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            placeholder="m@example.com"
+            value={email}
+            error={status === "error"}
+            helperText={
+              status === "error"
+                ? "invalid Email Format"
+                : undefined
+            }
+            onChange={(e) =>
+              setEmail(e.target.value)
+            }
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            placeholder="password"
+            value={password}
+            error={status === "error"}
+            helperText={
+              status === "error"
+                ? "invalid Email Format"
+                : undefined
+            }
+            onChange={(e) =>
+              setPassword(e.target.value)
+            }
+          />
+        </div>
+      </div>
+    </form>
+  </CardContent>
+  <CardFooter className="flex flex-col gap-2">
+    <Button
+      variant="primary"
+      size="sm"
+      className="w-full"
+      disabled={status === "loading"}
+      status={status}
+      onClick={handleLogin}
+    >
+      로그인
+    </Button>
+    <IconButton
+      label="Login with Kakao"
+      icon={<KakaoIcon />}
+      className="border-yellow w-full bg-yellow-500 text-sm text-black hover:bg-yellow-600 active:bg-yellow-700"
+    />
+  </CardFooter>
+</Card>
+          `}
         />
       </DocumentationSection>
       <DocumentationSection id="fail-login-input">
@@ -271,7 +336,72 @@ export default function InputPage() {
           }
           code={`import { Input } from "@yundeving/story-book-demo-ui";
 
-<Input type="email" placeholder="Email" error helperText="invalid Email Format" />`}
+<Card className="w-full max-w-sm">
+  <CardHeader>
+    <CardTitle>Login to your account</CardTitle>
+    <CardDescription>
+      Enter your email below to login to your account
+    </CardDescription>
+  </CardHeader>
+  <CardContent className="flex flex-col gap-2">
+    <form>
+      <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            placeholder="m@example.com"
+            value={email}
+            error={status === "error"}
+            helperText={
+              status === "error"
+                ? "invalid Email Format"
+                : undefined
+            }
+            onChange={(e) =>
+              setEmail(e.target.value)
+            }
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            placeholder="password"
+            value={password}
+            error={status === "error"}
+            helperText={
+              status === "error"
+                ? "invalid Email Format"
+                : undefined
+            }
+            onChange={(e) =>
+              setPassword(e.target.value)
+            }
+          />
+        </div>
+      </div>
+    </form>
+  </CardContent>
+  <CardFooter className="flex flex-col gap-2">
+    <Button
+      variant="primary"
+      size="sm"
+      className="w-full"
+      disabled={status === "loading"}
+      status={status}
+      onClick={handleLogin}
+    >
+      로그인
+    </Button>
+    <IconButton
+      label="Login with Kakao"
+      icon={<KakaoIcon />}
+      className="border-yellow w-full bg-yellow-500 text-sm text-black hover:bg-yellow-600 active:bg-yellow-700"
+    />
+  </CardFooter>
+</Card>
+          `}
         />
       </DocumentationSection>
     </DocumentationLayout>
