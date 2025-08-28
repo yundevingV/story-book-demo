72f30e6a53de9f5986d6fe0d7b518434d54ccb7e
yundevingV <91381230+yundevingV@users.noreply.github.com>
2025-08-28 16:59:54 +0900

    refactor: Simplify HorizonSkeleton component by using mapping for opacity classes

--- storybook-demo-test-app/src/app/skeleton/components/HorizonSkeleton.tsx
+++ storybook-demo-test-app/src/app/skeleton/components/HorizonSkeleton.tsx
@@ -1,28 +1,25 @@
 import { Skeleton } from "@yundeving/story-book-demo-ui";
 
 export default function HorizonSkeleton() {
+  const opacitiyList = [
+    "opacity-100",
+    "opacity-80",
+    "opacity-60",
+    "opacity-40",
+    "opacity-10",
+  ];
+
   return (
     <div className="flex flex-col gap-4">
-      <div className="flex items-center gap-2 opacity-100">
-        <Skeleton className="size-8" />
-        <Skeleton className="h-8 w-48" />
-      </div>
-      <div className="flex items-center gap-2 opacity-80">
-        <Skeleton className="size-8" />
-        <Skeleton className="h-8 w-48" />
-      </div>
-      <div className="flex items-center gap-2 opacity-60">
-        <Skeleton className="size-8" />
-        <Skeleton className="h-8 w-48" />
-      </div>
-      <div className="flex items-center gap-2 opacity-40">
-        <Skeleton className="size-8" />
-        <Skeleton className="h-8 w-48" />
-      </div>
-      <div className="flex items-center gap-2 opacity-10">
-        <Skeleton className="size-8" />
-        <Skeleton className="h-8 w-48" />
-      </div>
+      {opacitiyList.map((opacityClass) => (
+        <div
+          key={opacityClass}
+          className={`flex items-center gap-2 ${opacityClass}`}
+        >
+          <Skeleton className="size-8" />
+          <Skeleton className="h-8 w-48" />
+        </div>
+      ))}
     </div>
   );
 }
