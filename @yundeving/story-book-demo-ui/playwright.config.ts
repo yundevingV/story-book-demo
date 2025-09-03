573ed06c000f9894da213a09a6e2484d087208dd
yundevingV <91381230+yundevingV@users.noreply.github.com>
2025-09-03 14:37:52 +0900

    feat: Integrate Playwright for testing Component UI

---
+++ @yundeving/story-book-demo-ui/playwright.config.ts
@@ -0,0 +1,41 @@
+// playwright.config.ts
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+
+  use: {
+    baseURL: "http://localhost:6006",
+    trace: "on-first-retry",
+    screenshot: "only-on-failure",
+  },
+
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+  ],
+
+  // Storybook 자동 실행
+  // webServer: {
+  //   command: "yarn storybook",
+  //   url: "http://localhost:6006",
+  //   reuseExistingServer: !process.env.CI,
+  //   timeout: 120 * 1000,
+  //   cwd: process.cwd(),
+  // },
+});
