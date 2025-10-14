b92c1eac775db50e7490dd1e2d6a1c99ed0f4fbb
yundevingV <91381230+yundevingV@users.noreply.github.com>
2025-10-14 16:21:33 +0900

    feat: Add Accessibility Test code for Typography

---
+++ @yundeving/story-book-demo-ui/tests/accessibility/Typography/h4-typography.spec.ts
@@ -0,0 +1,23 @@
+import { deleteCSS } from "../../helper/deleteCSS";
+import { initPage } from "../../helper/initPage";
+import { waitFunction } from "../../helper/waitFunction";
+import AxeBuilder from "@axe-core/playwright";
+import { expect, test } from "@playwright/test";
+
+test.describe("Typography H4 접근성 테스트", () => {
+  test("Typography H4 접근성 위반사항 없어야 함", async ({ page }) => {
+    await initPage(page);
+
+    await page.goto("/iframe.html?id=typography--h-4");
+
+    await waitFunction(page);
+
+    await deleteCSS(page);
+
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .withTags(["wcag2a", "wcag2aa", "wcag21aa"])
+      .analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
